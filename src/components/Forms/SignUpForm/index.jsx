c73d58fb35990e26c26d639ba1fe6c6352bf4658
TannerB <tannerpace@gmail.com>
2021-10-26 12:10:40 -0400

    adds form validation

--- src/components/Forms/SignUpForm/index.jsx
+++ src/components/Forms/SignUpForm/index.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { userSignUp } from "../../../actions/User/users"
 import { useMutation } from "react-query"
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object({
+    userName: yup.string().required(),
+    password: yup.string()
+        .required('No password provided.')
+        .min(8, 'Password is too short - should be 8 chars minimum.')
+        .matches(/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/, 'Password must contain one uppercase one Special character a number and a letter.')
+}).required();
 export default function SighUpForm() {
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm({ resolver: yupResolver(schema) });
     const signUpMutation = useMutation(userSignUp)
     const onSubmit = (data) => {
         console.log(data)
@@ -11,11 +21,11 @@ export default function SighUpForm() {
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("userName", { required: true })} />
-            {errors.firstName?.type === 'required' && "Username is required"}
+            <input {...register("userName")} />
+            <p>{errors.userName?.message}</p>
 
-            <input {...register("password", { required: true })} />
-            {errors.password && "Password is required"}
+            <input {...register("password")} />
+            <p>{errors.password?.message}</p>
 
             <input type="submit" />
         </form>
