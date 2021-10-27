1cf30b048b117d0cc6affe3e8ca2ffed241c27a7
TannerB <tannerpace@gmail.com>
2021-10-27 12:03:34 -0400

    adds dummy components

---
+++ src/components/MessagesList/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MessagesList() {
+    return (
+        <div>
+            MessagesList Works!
+        </div>
+    )
+}
---
+++ src/components/UserMyAccount/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserMyAccount() {
+    return (
+        <div>
+            User My Account works
+        </div>
+    )
+}
